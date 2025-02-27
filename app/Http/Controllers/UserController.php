b3c44eb3d1034cd14858961c19613fcd9b1be991
sirfara <aratihsirf@gmail.com>
2025-02-27 17:59:20 +0700

    update

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /*public function index(){
+    $user = UserModel::all();
+    return view('user', ['data' => $user]);
+    }*/
+
+    public function index()
+{
+    // tambah data user dengan Eloquent Model
+    $data = [
+        'nama' => 'Pelanggan Pertama',
+    ];
+    UserModel::where('username', 'customer-1')->update($data); // update data user
+
+    // coba akses model UserModel
+    $user = UserModel::all(); // ambil semua data dari tabel m_user
+    return view('user', ['data' => $user]);
+}
+
+}
