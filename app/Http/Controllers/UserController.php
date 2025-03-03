c0b79139f6621c280a9af996dbd1af1b2444eaf7
sirfara <aratihsirf@gmail.com>
2025-03-03 11:24:29 +0700

    m4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,16 +14,18 @@ class UserController extends Controller
     }*/
 
     public function index()
-{
-    // tambah data user dengan Eloquent Model
-    $data = [
-        'nama' => 'Pelanggan Pertama',
-    ];
-    UserModel::where('username', 'customer-1')->update($data); // update data user
+    {
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
 
-    // coba akses model UserModel
-    $user = UserModel::all(); // ambil semua data dari tabel m_user
-    return view('user', ['data' => $user]);
-}
-
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
 }
