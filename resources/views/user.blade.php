7ab643890877f73dcba930a754586bf1658f0276
sirfara <aratihsirf@gmail.com>
2025-03-03 13:55:12 +0700

    m4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,11 +7,18 @@
         <h1><strong>Data User</strong></h1>
         <table border="1" cellpadding="5" cellspacing="0">
             <tr>
-                <th>Jumlah Pengguna</th>
-            </tr>
-            <tr>
-                <td>{{ $data }}</td>
-            </tr>
+                    <td>ID</td>
+                    <td>Username</td>
+                    <td>Nama</td>
+                    <td>ID Level Pengguna</td>
+                
+                <tr>
+                    <td>{{ $data->user_id }}</td>
+                    <td>{{ $data->username }}</td>
+                    <td>{{ $data->nama }}</td>
+                    <td>{{ $data->level_id }}</td>
+                </tr>
+                
         </table>
     </body>
 </html>
