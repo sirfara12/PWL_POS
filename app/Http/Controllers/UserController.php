63c6087cfd5bf59f22ebcb03f5001479aa2de04b
sirfara <aratihsirf@gmail.com>
2025-03-03 12:07:56 +0700

    m4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,29 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
+use Illuminate\Database\Capsule\Manager;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /*public function index(){
-    $user = UserModel::all();
-    return view('user', ['data' => $user]);
-    }*/
-
-    public function index()
-    {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+    public function index() {
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
