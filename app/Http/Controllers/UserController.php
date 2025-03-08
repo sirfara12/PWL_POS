c0b4cf22e556dce0b0fe98f5248227c63bfcd920
sirfara <aratihsirf@gmail.com>
2025-03-08 13:36:22 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,12 +6,13 @@ use App\Models\UserModel;
 use Illuminate\Database\Capsule\Manager;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Psy\Readline\Userland;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create([
+       /* $user = UserModel::create([
             'username' => 'manager11',
             'nama' => 'Manager11',
             'password' => Hash::make('12345'),
@@ -26,6 +27,46 @@ class UserController extends Controller
         $user->wasChanged('username'); // true
         $user->wasChanged(['username', 'level_id']); // true
         $user->wasChanged('nama'); // false
-        dd($user->wasChanged(['nama', 'username'])); // true
+        dd($user->wasChanged(['nama', 'username'])); // <true></true>*/
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+
     }
+    public function tambah(){
+        return view('user_tambah');
+    }
+    public function tambah_simpan(Request $request){
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->namespace,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
+
+        ]);
+        return redirect('/user');
+    }
+    public function ubah($id){
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+    }
+    public function hapus($id){
+        $user = UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
+    }
+
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,21 +4,31 @@
         <title>Data User</title>
     </head>
     <body>
-        <h1><strong>Data User</strong></h1>
-        <table border="1" cellpadding="5" cellspacing="0">
-            <tr>
-                    <td>ID</td>
-                    <td>Username</td>
-                    <td>Nama</td>
-                    <td>ID Level Pengguna</td>
-                
-                <tr>
-                    <td>{{ $data->user_id }}</td>
-                    <td>{{ $data->username }}</td>
-                    <td>{{ $data->nama }}</td>
-                    <td>{{ $data->level_id }}</td>
-                </tr>
-                
-        </table>
-    </body>
+        <h1>Form Ubah Data User</h1>
+        <a href="/user">Kembali</a>
+    
+        <form method="post" action="/user/ubah_simpan/{{ $data->first()->user_id }}">
+    
+            {{ csrf_field() }}
+            {{ method_field("PUT") }}
+    
+            <label>Username</label>
+            <input type="text" name="username" placeholder="Masukan Username" value="{{ $data->first()->username }}">
+            <br>
+    
+            <label>Nama</label>
+            <input type="text" name="nama" placeholder="Masukan Nama" value="{{ $data->first()->username }}">
+            <br>
+    
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Masukan Password" value="{{ $data->first()->password }}">
+            <br>
+    
+            <label>Level ID</label>
+            <input type="number" name="level_id" placeholder="Masukan ID Level" value="{{ $data->first()->level_id }}">
+            <br><br>
+    
+            <input type="submit" class="btn btn-success" value="Ubah">
+        </form>
+    </body>    
 </html>
