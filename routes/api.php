f06e136f89d98fa9c9e857a1e1e0ef9f621a5476
sirfara <aratihsirf@gmail.com>
2025-04-14 20:38:58 +0700

    Praktikum 3, jb 10

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
@@ -19,6 +20,7 @@ use App\Http\Controllers\Api\RegisterController;
 
 Route::post('/register', [RegisterController::class, 'post'])->name('register');
 Route::post('/login', [LoginController::class, 'post'])->name('login');
+Route::post('/logout', [LogoutController::class, 'post'])->name('logout');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
